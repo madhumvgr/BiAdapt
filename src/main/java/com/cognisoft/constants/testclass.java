e01e642f30ce25e551e10a6b0f6eac48df5b2961
Sreedhar.G <sreedhar@cognisofttech.com>
2017-07-29 23:39:51 +0530

    test class modified

--- src/main/java/com/cognisoft/constants/testclass.java
+++ src/main/java/com/cognisoft/constants/testclass.java
@@ -3,7 +3,7 @@ package com.cognisoft.constants;
 public class testclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("greetings..!");
 
 	}
 
